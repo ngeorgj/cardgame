9a1732d1f858ca33e6c8f7a4b06549201553d7c1
Nivaldo <ngj.netrunner@gmail.com>
2020-12-25 01:52:32 -0300

    minor changes

--- engine/game/cards/collection/expansion_sets/genesis/expansion_class.py
+++ engine/game/cards/collection/expansion_sets/genesis/expansion_class.py
@@ -10,5 +10,5 @@ class Genesis:
 
     expansion_details = 'The first cards are in this expansion. 2020 is the year of the COVID pandemic.'
 
-    card_creator   = 'Ana Vitória Selista'
-    card_developer = 'Nivaldo Georg Junior'
+    card_creator   = 'CardCreator'
+    card_developer = 'NGJ'
